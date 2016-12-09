c4d37401d7a7062281b17c9e70737c29a7232973
Yannik Behr <yannikbehr@yanmail.de>
2016-12-09 14:45:42 +1300

    adding setup script

---
+++ setup.py
@@ -0,0 +1,22 @@
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup
+
+####################################################################
+#                    CONFIGURATION
+####################################################################
+
+# do the build/install
+setup(
+    name="clemb",
+    version="0.0.1",
+    description="Python package to compute crater lake energy and mass balance.",
+    long_description="Python package to compute crater lake energy and mass balance.",
+    author="Yannik Behr",
+    author_email="yannikbehr@yanmail.de",
+    url="",
+    license="GPL v3",
+    package_dir={'': 'src'},
+    install_requires=['pandas', 'numpy'],
+    packages=['clemb']
+)
