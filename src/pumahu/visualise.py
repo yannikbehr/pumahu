0567e5a81cd05628f8f97514ea8f89906a191a6c
Yannik Behr <y.behr@gns.cri.nz>
2021-03-05 13:07:58 +1300

    enable https request

--- src/pumahu/visualise.py
+++ src/pumahu/visualise.py
@@ -5,6 +5,7 @@ computations.
 from collections import defaultdict
 import os
 import urllib
+import ssl
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -227,11 +228,17 @@ def mcmc_heat_input(data, filename=None):
 
 
 def get_rsam_data(filename, outdir):
+    # don't verify the certificate
+    myssl = ssl.create_default_context();
+    myssl.check_hostname=False
+    myssl.verify_mode=ssl.CERT_NONE
+
     baseurl = 'https://volcanolab.gns.cri.nz:8082/rsam/MAVZ.NZ/'
     if not os.path.isdir(outdir):
         os.makedirs(outdir)
     fout = os.path.join(outdir, filename)
-    hr = urllib.request.urlopen(os.path.join(baseurl, filename))
+    hr = urllib.request.urlopen(os.path.join(baseurl, filename),
+                                context=myssl)
     with open(fout, 'wb') as fh:
         fh.write(hr.read())
     return fout
