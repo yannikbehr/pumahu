95570790aa27ade79d202746bf09f4ab58f8a5c3
Yannik Behr <y.behr@gns.cri.nz>
2021-03-29 16:34:26 +1300

    major refactoring

--- src/pumahu/syn_model.py
+++ src/pumahu/syn_model.py
@@ -1,3 +1,4 @@
+import hashlib
 from datetime import date, timedelta
 
 from cachier import cachier
@@ -9,6 +10,139 @@ import xarray as xr
 from tqdm import tqdm
 
 from .forward_model import Forwardmodel
+
+
+def setup_test(q_in=1000.):
+    """
+    A synthetic scenario for unit testing.
+
+    :param q_in: Maximum heat input rate [MW]
+    :type q_in: float
+    :returns: An xarray DataFrame containing the input
+              data for the synthetic model
+    :rtype: :class:`xarray.DataArray`
+    """
+    dates = pd.date_range(start='2017-01-01', end='2017-01-04',
+                          freq='D')
+    data = np.zeros((4,4))
+    params = ['q_in', 'm_in', 'h', 'W']
+    # qi
+    data[0, :] = np.array([0., .2, .6, 1.])*q_in
+    # Mi
+    data[1, :] = np.ones(4)*10.
+    # H
+    data[2, :] = np.ones(4)*6
+    # W
+    data[3, :] = np.ones(4)*4.5
+    return xr.DataArray(data, coords=[('params', params),
+                                      ('times', dates)])
+
+
+def setup_realistic(q_in=1000., tmax=90., sinterval=10):
+    """
+    Synthetic scenario where the heat influx is a
+    combination of 3rd order polynomial and sinusoid.
+
+    :param q_in: Maximum heat input rate [MW]
+    :type q_in: float
+    :param tmax: Length of the time-series [days]
+    :type tmax: float
+    :param sinterval: Sampling interval [mins]
+    :type sinterval: int
+    :returns: An xarray DataFrame containing the input
+              data for the synthetic model
+    :rtype: :class:`xarray.DataArray`
+    """
+    dt = (sinterval * 60)/86400.
+    t = np.arange(0,tmax+dt, dt)
+    dates = pd.date_range(start='2020-03-03', freq='%dmin'%sinterval,
+                          periods=t.size)
+    a = 1.
+    b = -20
+    c = b**2/4+3
+    y = 1./(a*t**2 + b*t + c)
+    y = q_in*y/max(y)
+
+    npts = y.size
+    y_last = y[npts//2]
+    f = 1./20.
+    t1 = t[npts//2::]
+    y[npts//2:] = np.sin(2.*np.pi*f*t1 + 2*np.pi)*q_in/4.+q_in/4.
+    y[npts//2:] *= tukey(t1.size, 0.9)
+    y[npts//2:] += y_last
+    
+    data = np.zeros((4,t.size))
+    params = ['q_in', 'm_in', 'h', 'W']
+    # qi
+    data[0, :] = y
+    # Mi
+    data[1, :] = np.ones(t.size)*10.
+    # H
+    data[2, :] = np.ones(t.size)*6
+    # W
+    data[3, :] = np.ones(t.size)*4.5
+    return xr.DataArray(data, coords=[('params', params),
+                                      ('times', dates)])
+#        if mode == 'sinus':
+#            qi = np.sin(2.*np.pi*self.f*t + np.deg2rad(15))*q_in/2.+q_in/2.
+#            qi *= tukey(nsteps, 0.9)
+#        if mode == 'tukey':
+#            tail = int(10/dt)
+#            qi = np.ones(nsteps-tail)*q_in
+#            qi *= tukey(nsteps-tail, 0.9)
+#            qi_tmp = np.zeros(nsteps)
+#            qi_tmp[0:nsteps-tail] = qi
+#            qi = qi_tmp
+#        if mode == 'step':
+#            tail = int(10/dt)
+#            qi = np.ones(nsteps-tail)*q_in
+#            qi_tmp = np.zeros(nsteps)
+#            qi_tmp[0:nsteps-tail] = qi
+#            qi = qi_tmp
+#        if mode == 'constant':
+#            qi = np.ones(nsteps)*q_in
+#        if mode == 'gamma':
+#            y = gamma.pdf(t, 3., loc=4)
+#            y /= y.max()
+#            y *= q_in
+#            qi = y
+#
+#        if mode == 'observed':
+#            # a polynomial model of a real inversion
+#            z = np.array([-6.93795125e-30,  1.17673746e-26, -9.19940930e-24,
+#                          4.39229128e-21, -1.43037875e-18,  3.36004438e-16,
+#                          -5.87098984e-14,  7.75486512e-12, -7.79213224e-10,
+#                          5.94657977e-08, -3.41657063e-06,  1.45392771e-04,
+#                          -4.46901292e-03,  9.56540064e-02, -1.35189993e+00,
+#                          1.16488040e+01, -5.37668791e+01,  1.05017651e+02,
+#                          2.91772946e+01])
+#            p = np.poly1d(z)
+#            dates = pd.date_range(start='2018-4-1', end='2018-9-30',
+#                                  periods=183)
+#            t = np.r_[0, np.cumsum(np.diff(dates)/np.timedelta64(1, 'D'))]
+#            qi = p(t)
+#            nsteps = 183
+#
+#        if mode == 'test':
+#            qi = np.array([0., .2, .6, 1.])*q_in
+#            dates = pd.date_range(start='2017-01-01', end='2017-01-04',
+#                                  freq='D')
+#            nsteps = 4
+#            
+def myhash(args, kwds):
+    key = []
+    key.append(args[0].integration_method)
+    for a in args[1:]:
+        if isinstance(a, xr.core.dataarray.DataArray):
+            key.append(hashlib.sha256(a.values).hexdigest())
+        elif isinstance(a, list):
+            key.append(tuple(a))
+        else:
+            key.append(a)
+
+    key = tuple(key)
+    key += tuple(sorted(kwds.items()))
+    return key
 
 
 class SynModel:
@@ -28,6 +162,7 @@ class SynModel:
         self.tmax = 30.
         self.a = area
         self.T0 = T0
+        self.integration_method=integration_method
         if seed is not None:
             np.random.seed(seed)
         self.fm = Forwardmodel(method=integration_method,
@@ -80,163 +215,108 @@ class SynModel:
         v = self.volume(mass, temp)
         return self.a, v
 
-    def run(self, q_in, mode='gamma', nsteps=100, gradient=False,
-            addnoise=False, 
+    @cachier(stale_after=timedelta(weeks=2),
+             cache_dir='.cache', hash_params=myhash)
+    def run(self, data, gradient=False, addnoise=False, 
             estimatenoise=False):
         """
         Produce synthetic observations.
         """
-        np.random.seed(42)
-        dates = pd.date_range(start='2017-01-01', end='2017-01-21',
-                              periods=nsteps)
-        t = np.linspace(0, self.tmax, nsteps)
-        dt = (dates[1] - dates[0])/pd.Timedelta('1D')
-        if mode == 'sinus':
-            qi = np.sin(2.*np.pi*self.f*t + np.deg2rad(15))*q_in/2.+q_in/2.
-            qi *= tukey(nsteps, 0.9)
-        if mode == 'tukey':
-            tail = int(10/dt)
-            qi = np.ones(nsteps-tail)*q_in
-            qi *= tukey(nsteps-tail, 0.9)
-            qi_tmp = np.zeros(nsteps)
-            qi_tmp[0:nsteps-tail] = qi
-            qi = qi_tmp
-        if mode == 'step':
-            tail = int(10/dt)
-            qi = np.ones(nsteps-tail)*q_in
-            qi_tmp = np.zeros(nsteps)
-            qi_tmp[0:nsteps-tail] = qi
-            qi = qi_tmp
-        if mode == 'constant':
-            qi = np.ones(nsteps)*q_in
-        if mode == 'gamma':
-            y = gamma.pdf(t, 3., loc=4)
-            y /= y.max()
-            y *= q_in
-            qi = y
-
-        if mode == 'observed':
-            # a polynomial model of a real inversion
-            z = np.array([-6.93795125e-30,  1.17673746e-26, -9.19940930e-24,
-                          4.39229128e-21, -1.43037875e-18,  3.36004438e-16,
-                          -5.87098984e-14,  7.75486512e-12, -7.79213224e-10,
-                          5.94657977e-08, -3.41657063e-06,  1.45392771e-04,
-                          -4.46901292e-03,  9.56540064e-02, -1.35189993e+00,
-                          1.16488040e+01, -5.37668791e+01,  1.05017651e+02,
-                          2.91772946e+01])
-            p = np.poly1d(z)
-            dates = pd.date_range(start='2018-4-1', end='2018-9-30',
-                                  periods=183)
-            t = np.r_[0, np.cumsum(np.diff(dates)/np.timedelta64(1, 'D'))]
-            qi = p(t)
-            nsteps = 183
-
-        if mode == 'test':
-            qi = np.array([0., .2, .6, 1.])*q_in
-            dates = pd.date_range(start='2017-01-01', end='2017-01-04',
-                                  freq='D')
-            nsteps = 4
-            
-        if mode == 'gamma+sin':
-            y = gamma.pdf(t, 3., loc=4)
-            y /= y.max()
-            y *= q_in
-            npts = y.size
-            t1 = t[npts//2::]
-            t1 = np.arange(t1.size)
-            y[npts//2:] = np.sin(2.*np.pi*self.f*t1 + 2*np.pi)*q_in/4.+q_in/4.
-            y[npts//2:] *= tukey(t1.size, 0.9)
-            qi = y
-        y = np.zeros((nsteps, 8))
-        prm = np.zeros((nsteps, 2))
+        prms = ['T', 'M', 'X', 'q_in',
+                'm_in', 'm_out', 'h', 'W',
+                'p', 'A', 'V', 'z', 'Mg']
+        nprms = len(prms)
+        nsteps = data.times.size
+        dates = pd.to_datetime(data['times'].values)
+        qi = data.loc[dict(params='q_in')].values
+        exp = np.zeros((nsteps, nprms, 2))
+        error = {'T': 0.4, 'z': 0.01, 'M': 2.,
+                 'X': 0.4, 'A': 30., 'V': 2.0,
+                 'Mg': 50, 'm_out': .25, 'W':.5}
+ 
         V = 8800
         A = self.a
-        Mi = 10.
-        H = 6.0
-        ws = 4.5
         X = 2.
         M = self.mass(V, self.T0)
         ll = self.level(V, A)
         Mo = self.outflow(ll)
-        y[0, :] = [self.T0, M, X, qi[0]*0.0864,
-                   Mi, Mo, H, ws]
-        for i in range(nsteps-1):
-            prm[i, 0] = ll
+        exp[0, 0:3, 0] = [self.T0, M, X]
+        exp[0, prms.index('V'), 0] = V 
+        exp[0, prms.index('z'), 0] = ll
+        exp[0, prms.index('A'), 0] = A 
+        Mg = exp[0, prms.index('X'), 0]/exp[0, prms.index('M'), 0]*1e6
+        exp[0, prms.index('Mg'), 0] = Mg
+        p = 1.003 - 0.00033 * exp[0, prms.index('T'), 0]
+        exp[0, prms.index('p'), 0] = p
+        
+        for i in range(nsteps - 1):
+            exp[i, prms.index('h'), 0] = data.isel(times=i).loc['h']
+            exp[i, prms.index('W'), 0] = data.isel(times=i).loc['W'] 
+            exp[i, prms.index('m_in'), 0] = data.isel(times=i).loc['m_in'] 
+            exp[i, prms.index('q_in'), 0] = 0.0864*qi[i]
             dt = (dates[i+1] - dates[i])/pd.Timedelta('1D')
+
             if not gradient:
                 dp = [0.] * 5
-                y[i, 3] = qi[i]*0.0864
-                y[i, 5] = Mo
             else:
                 dp = [0.] * 5
                 dp[0] = (qi[i+1] - qi[i])*0.0864/dt
-                y[i, 5] = Mo
-            y_new = self.fm.integrate(y[i], dates[i], dt, dp)
-            prm[i, 1] = self.fm.get_evap()[1]
+            y_new = self.fm.integrate(exp[i, 0:8, 0],
+                                      dates[i], dt, dp)
             V = self.volume(y_new[1], y_new[0])
             ll = self.level(V, A)
             Mo = self.outflow(ll)
-            y[i+1, :] = y_new.copy()
-        prm[i+1, 0] = ll
-        prm[i+1, 1] = self.fm.get_evap()[1]
-
-        # Prescripe errors
-        factor = 1.
-        T_err = factor*0.4*dt
-        z_err = factor*0.01*dt
-        M_err = factor*2.*dt
-        X_err = factor*0.4*dt
-        A_err = factor*30.*dt
-        V_err = factor*2.0*dt
-        Mg_err = factor*50*dt
-        Mo_err = factor*.25
-        W_err = factor*.5
-
-        # Design dataset
-        syn_data = {}
-        syn_data['T'] = y[:, 0]
-        syn_data['T_err'] = np.ones(nsteps)*T_err
-        syn_data['p_mean'] = 1.003 - 0.00033 * syn_data['T']
-        syn_data['p_err'] = 0.00033*np.random.normal(scale=T_err, size=nsteps)
-        syn_data['a'] = y[:, 6]
-        syn_data['a_err'] = np.ones(nsteps)*A_err
-        syn_data['M'] = y[:, 1]
-        syn_data['M_err'] = np.ones(nsteps)*M_err
-        syn_data['v'] = self.volume(syn_data['M'], syn_data['T'])
-        syn_data['v_err'] = V_err
-        syn_data['X'] = y[:, 2]
-        syn_data['X_err'] = np.ones(nsteps)*X_err
-        syn_data['Mg'] = syn_data['X']/syn_data['M']*1e6
-        syn_data['Mg_err'] = Mg_err
-        syn_data['z'] = prm[:, 0]
-        syn_data['z_err'] = np.ones(nsteps)*z_err
-        syn_data['W'] = np.ones(nsteps)*4.5
-        syn_data['W_err'] = np.ones(nsteps)*W_err
-        syn_data['H'] = np.ones(nsteps)*6.0
-        syn_data['dv'] = np.ones(nsteps)*1.0
-        syn_data['Mi'] = y[:, 4]
-        syn_data['Mo'] = y[:, 5]
-        syn_data['Mo_err'] = Mo_err*y[:, 5]
-        syn_data['mevap'] = prm[:, 1]
-        syn_data['qi'] = y[:, 3]/0.0864
+            exp[i+1, 0:8, 0] = y_new.copy()
+            exp[i+1, prms.index('V'), 0] = V 
+            exp[i+1, prms.index('z'), 0] = ll
+            exp[i+1, prms.index('A'), 0] = A 
+            Mg = exp[i+1, prms.index('X'), 0]/exp[i+1, prms.index('M'), 0]*1e6
+            exp[i+1, prms.index('Mg'), 0] = Mg
+            p = 1.003 - 0.00033 * exp[i+1, prms.index('T'), 0]
+            exp[i+1, prms.index('p'), 0] = p
+            
+            # Prescripe errors
+            for k in error.keys(): 
+                exp[i+1, prms.index(k), 1] = error[k]
+        exp[:, prms.index('q_in'), 0] /= 0.0864
 
         if addnoise:
-            syn_data['T'] += np.random.normal(scale=T_err, size=nsteps)
-            syn_data['M'] += np.random.normal(scale=M_err, size=nsteps)
-            syn_data['X'] += np.random.normal(scale=X_err, size=nsteps)
+            for k in ['T', 'M', 'X', 'W']:
+                exp[:, prms.index(k), 0] += np.random.normal(scale=error[k],
+                                                             size=nsteps)
 
-        df = pd.DataFrame(syn_data, index=dates)
-        if estimatenoise:
-            df_mean = df.groupby(pd.Grouper(freq='D')).mean()
-            df_std = df.groupby(pd.Grouper(freq='D')).std()
-            df_mean['T_err'] = df_std['T']
-            df_mean['M_err'] = df_std['M']
-            df_mean['X_err'] = df_std['X']
-            df = df_mean
-
-        xds = xr.Dataset(df)
-        xds = xds.rename({'dim_0': 'dates'})
+        xds = xr.Dataset({'exp': (('dates', 'parameters', 'val_std'), exp)},
+                         {'dates': dates.values,
+                          'parameters': prms,
+                          'val_std': ['val', 'std']})
         return xds
+
+
+def resample(xds, parameters=['T', 'M', 'X', 'z', 'W'], sinterval='1D'):
+    """
+    Resample a synthetic model to a given sampling interval.
+    
+    Parameters:
+    -----------
+    xds : :class:`xarray.Dataset`
+          The input synthetic dataset.
+    parameters : array_like
+                 Parameters to resample
+    sinterval : str
+                The resampling interval. Currently, only downsampling
+                supported.
+    """
+    nxds = xds.loc[dict(parameters=parameters, val_std='val')].resample(dates=sinterval)
+    mn = nxds.mean().to_array().values
+    st = nxds.std().to_array().values
+    new_data = np.concatenate((mn[:,:,:,np.newaxis], st[:,:,:,np.newaxis]), axis=3)
+    rxds = xr.DataArray(new_data[0], dims=('dates', 'parameters', 'val_std'),
+                        coords=(nxds.mean().dates, parameters,['val', 'std']) )
+    # fill fields with zero variance with the average
+    # variance for the respective parameter
+    idx = np.where(rxds.loc[dict(val_std='std')] == 0.)
+    rxds[idx[0], idx[1], 1] = rxds[:, :, 1].mean(axis=0)
+    return rxds
 
 
 def lhs(dim, nsamples, centered=False):
