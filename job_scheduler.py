fda34a6f0282cfea34ab99f851d6d7d715e75f06
Yannik Behr <y.behr@gns.cri.nz>
2021-07-02 16:12:33 +1200

    bugfix

--- job_scheduler.py
+++ job_scheduler.py
@@ -4,7 +4,9 @@ import time
 
 def job():
     subprocess.run(['heat_mcmc', '--rdir', '/opt/data', '-f', '-p', '-d'])
-    subprocess.run(['heat_mcmc', '--rdir', '/opt/data', '-f', '-p', '-d'])
+    subprocess.run(['heat_uks', '--rdir', '/opt/data',
+                    '-f', '-p', '-d',
+                    '--benchmark', 'mcmc_sampling.nc'])
 
 schedule.every().day.at("11:00").do(job)
 
