8a9bf114d334f1778281526771ab909cb0199ee6
Yannik Behr <y.behr@gns.cri.nz>
2021-07-04 21:41:36 +0000

    Update job_scheduler.py

--- job_scheduler.py
+++ job_scheduler.py
@@ -6,7 +6,7 @@ def job():
     subprocess.run(['heat_mcmc', '--rdir', '/opt/data', '-f', '-p', '-d'])
     subprocess.run(['heat_uks', '--rdir', '/opt/data',
                     '-f', '-p', '-d',
-                    '--benchmark', 'mcmc_sampling.nc'])
+                    '--benchmark', '/opt/data/mcmc_sampling.nc'])
 
 schedule.every().day.at("11:00").do(job)
 
