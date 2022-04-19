88a5a67ac83d2d8ba338d92c55935d1b3d44b796
Yannik Behr <y.behr@gns.cri.nz>
2022-04-19 14:28:40 +1200

    Change to pywt to get rid of noise in contour lines

--- manuscript/figures/time_frequency_analysis.py
+++ manuscript/figures/time_frequency_analysis.py
@@ -1,3 +1,4 @@
+from tkinter import W
 import pandas as pd
 import numpy as np
 import os
@@ -8,29 +9,53 @@ import pywt
 
 from pumahu.visualise import TrellisPlot
 from pumahu.uks import main as main_uks
+import pycwt
+from pycwt.helpers import find
+
 
 if False:
     df_modis = pd.read_csv(get_data('data/41561_2021_705_MOESM2_ESM.csv'), usecols=[5, 6, 7, 8],
                             index_col=0, dayfirst=True, parse_dates=True)
     df_modis = df_modis.loc[str(dates[0]):str(dates[-1])]
     
-def wavelet_analysis(dates, data, nscales=128):
-    sst = data - data.mean()
-    dt = (dates[1]-dates[0])/pd.Timedelta('1D')
+def wavelet_analysis(dates, data, wavelet_pkg='pycwt'):
+    if wavelet_pkg == 'pywt':
+        nscales = 256
+        sst = data - data.mean()
+        dt = (dates[1]-dates[0])/pd.Timedelta('1D')
 
-    # Taken from http://nicolasfauchereau.github.io/climatecode/posts/wavelet-analysis-in-python/
-    wavelet = 'cmor1.5-1.0'
-    #wavelet = 'cmor2.0-6.0'
-    #wavelet = 'mexh'
-    scales = np.arange(1, nscales)
+        # Taken from http://nicolasfauchereau.github.io/climatecode/posts/wavelet-analysis-in-python/
+        wavelet = 'cmor1.5-1.0'
+        #wavelet = 'cmor2.0-6.0'
+        #wavelet = 'mexh'
+        scales = np.arange(1, nscales)
 
-    [cfs, frequencies] = pywt.cwt(sst, scales, wavelet, dt)
-    power = (abs(cfs)) ** 2
+        [cfs, frequencies] = pywt.cwt(sst, scales, wavelet, dt)
+        power = (abs(cfs)) ** 2
 
-    period = 1. / frequencies
+        period = 1. / frequencies
+    elif wavelet_pkg == 'pycwt':
+        dt = (np.diff(pd.to_datetime(dates))/pd.Timedelta('1D'))[0]
+        N = data.size
+        t = np.arange(0, N) * dt
+        p = np.polyfit(t, data, 1)
+        data_notrend = data - np.polyval(p, t)
+        std = data_notrend.std()  # Standard deviation
+        var = std ** 2  # Variance
+        data_norm = data_notrend / std  # Normalized dataset
+        mother = pycwt.Morlet(6)
+        s0 = 2 * dt  # Starting scale, in this case 2 * 0.25 years = 6 months
+        dj = 1 / 12  # Twelve sub-octaves per octaves
+        J = 8 / dj  # Seven powers of two with dj sub-octaves
+        alpha, _, _ = pycwt.ar1(data)  # Lag-1 autocorrelation for red noise
+        wave, scales, freqs, coi, fft, fftfreqs = pycwt.cwt(data_norm, dt, dj, s0, J,
+                                                      mother)
+        iwave = pycwt.icwt(wave, scales, dt, dj, mother) * std
+        power = (np.abs(wave)) ** 2
+        fft_power = np.abs(fft) ** 2
+        period = 1 / freqs
     return period, power
-
-   
+       
 fn = 'data/uks.nc'
 if not os.path.isfile(fn):
     main_uks(['--rdir', './data', '-s', '2016-03-04',
@@ -43,7 +68,7 @@ dates = xdf['dates'].values
 tp = TrellisPlot()
 ymean, ymin, ymax = tp.get_traces(xdf.exp, 'q_in')
 nscales = 256 
-period, power = wavelet_analysis(dates, data, nscales)
+period, power = wavelet_analysis(dates, data)
 mean_power = power.mean(axis=1)
 std_power = power.std(axis=1)
 min_power = mean_power - std_power
@@ -55,16 +80,14 @@ fig.add_trace(go.Contour(
         x = pd.to_datetime(dates),
         y = period,
         z = power,
-        contours=dict(
-            start=0,
-            end=700e3,
-            size=100e3,
-        ),
+        ncontours=7,
         contours_coloring='heatmap',
         colorscale = 'Blues',
         reversescale = True,
         xaxis = 'x',
-        yaxis = 'y'
+        yaxis = 'y',
+        line_smoothing=1.3,
+        connectgaps=True
     ))
 
 fig.add_trace(go.Scatter(
@@ -87,7 +110,8 @@ fig.add_trace(go.Scatter(
         yaxis = 'y2',
         marker = dict(
             color = 'rgba(0,0,0,1)'
-        )
+        ),
+        line_color = 'rgb(255,65,77)'
     ))
 fig.add_trace(go.Scatter(x=pd.to_datetime(dates),
                          y=ymin,
@@ -104,7 +128,7 @@ fig.add_trace(go.Scatter(x=pd.to_datetime(dates),
                          marker=dict(color="#444"),
                          line=dict(width=0),
                          showlegend=False,
-                         fillcolor='rgba(68, 68, 68, 0.3)',
+                         fillcolor='rgba(255, 65, 77, 0.3)',
                          fill='tonexty'))
 
 
@@ -147,7 +171,7 @@ fig.add_trace(go.Scatter(y=period,
                          fill='tonextx'))
 
 fig.add_trace(go.Scatter(
-    x=[100e3],
+    x=[30],
     y=[10],
     mode="text",
     name="Markers and Text",
@@ -166,22 +190,22 @@ fig.update_layout(
     ),
     yaxis = dict(
         zeroline = False,
-        domain = [0,0.8],
+        domain = [0,0.7],
         showgrid = True,
-        range = (nscales, 0),
+        range = np.array([nscales, 0]),
         title = 'Period [days]'
     ),
     xaxis2 = dict(
         zeroline = False,
         domain = [0.85,1],
         showgrid = True,
-        tickvals=[200e3, 400e3, 600e3],
+        tickvals=[20, 40, 60],
         tickmode='array',
-        range = (0, 600e3)
+        range = (0, 70)
     ),
     yaxis2 = dict(
         zeroline = False,
-        domain = [0.8,1],
+        domain = [0.7,1],
         showgrid = True,
         tickvals=[200, 600, 1000],
         tickmode='array',
