16ec50a094ea6bcef80aa768fec4d7132da8876c
Yannik Behr <y.behr@gns.cri.nz>
2022-09-30 17:16:07 +1300

    remove obsolete imports

--- src/pumahu/__init__.py
+++ src/pumahu/__init__.py
@@ -8,4 +8,4 @@ from .forward_model import Forwardmodel
 from .mcmc import ns_sampling 
 from .syn_model import SynModel
 from .uks import UnscentedKalmanSmoother
-from .data import LakeData
+from .data import LakeData, get_MetService_wind, get_outflow
